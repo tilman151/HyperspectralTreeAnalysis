77ca9ed1b1171895a6daf315fd205db8f85468fa
Cornelius Styp von Rekowski <cornelius.vonrekowski@gmail.com>
2016-11-23 23:44:21 +0100

    Create function for extracting labeled pixels
    This function extracts a list of labeled pixels and their corresponding labels
    from the spatial maps. The function is located in a newly created utility
    directory.

---
+++ code/util/extractLabeledPixels.m
@@ -0,0 +1,27 @@
+function [featureList, labelList] = ...
+    extractLabeledPixels(featureMap, labelMap)
+%EXTRACTLABELEDPIXELS Extract labeled pixels from the given feature map
+%   Produce the list of labeled pixels and their corresponding labels from
+%   the given spatial maps. The information on their location is lost in
+%   that step. Unlabeled pixels and empty pixels that were used to fill the
+%   image are dropped.
+
+% Logical positions of all labeled pixels, excluding unlabeled and empty 
+% pixels (0 and -1)
+labeledPos = labelMap > 0;
+
+% Extract list of labels
+labelList = labelMap(labeledPos);
+
+% Create empty feature list
+numLabels = nnz(labeledPos);
+numFeatures = size(featureMap, 3);
+featureList = zeros(numLabels, numFeatures);
+
+% For each feature dimension, extract values for the labeled pixels
+for feature = 1:numFeatures
+    singleFeatureMap = featureMap(:, :, feature);
+    featureList(:, feature) = singleFeatureMap(labeledPos);
+end
+
+end
