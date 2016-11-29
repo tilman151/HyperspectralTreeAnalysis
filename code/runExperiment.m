31149cccf56118fb8383a2b26209229e8ef698bb
Tilman Krokotsch <t.krokotsch@gmail.com>
2016-11-29 21:48:29 +0100

    Added documentation for Importer and runExperiment; set confusion matrix as output of runExperiment; added spaces after commas

--- code/runExperiment.m
+++ code/runExperiment.m
@@ -1,17 +1,40 @@
-function runExperiment(configFilePath)
+function cfMat = runExperiment(configFilePath)
     %RUNEXPERIMENT Run a machine learning experiment
-    %with the parameters defined in the configuration file
-
-    %Add the code directory and all subdirectories to path
+    %              with the parameters defined in the configuration file
+    %
+    %   The function takes a path to a configuration file and reads it. The
+    %   statements in the file are executed to initialize the needed
+    %   variables for the experiment.
+    %   
+    %   The data set is read via the Importer class. The classes of the
+    %   labels are extracted and a cross validation partition is
+    %   calculated. Afterwards cross validation is performed with the
+    %   classifier specified in the config file to obtain a confusion
+    %   matrix.
+    %
+    %%  Input:
+    %       configFilePath ... a path to the config file file
+    %
+    %%  Output:
+    %       cfMat ............ a confusion matrix yielded by cross
+    %                          validation using the classifier and the data
+    %                          set defined in the config file.
+    %
+    % Version: 2016-11-29
+    % Author: Tilman Krokotsch
+    %%
+    
+    % Add the code directory and all subdirectories to path
     addpath(genpath('./'));
 
+    % Read and execute config file
     file = fopen(configFilePath);
     config = textscan(file, '%s', 'whitespace', '\n');
     cellfun(@eval, config{1});
 
-    %TODO: Check config validity
+    % TODO: Check config validity
 
-    %Read csv or mat/png file from dataSetPath
+    % Read csv or mat/png file from dataSetPath
     [labels, features] = Importer.loadDataFrom(dataSetPath);    
 
     % Order of the group labels for cinfusion matrix
@@ -27,15 +50,35 @@ function runExperiment(configFilePath)
     % Run cross validation
     cfMat = crossval(func, features, labels, 'partition', cp);
     % compute confusion matrix over all cross validation instances
-    cfMat = reshape(sum(cfMat), size(order, 1), size(order, 1))
+    cfMat = reshape(sum(cfMat), size(order, 1), size(order, 1));
 
 end
 
 function confMat = crossValFunc(trainFeatures, trainLabels, ...
                                 evalFeatures, evalLabels, ...
                                 order, classifier)
-    %crossValFunc Function for the matlab cross validation.
-    %Has to be curried to bind order and classifier beforehands
+    %CROSSVALFUNC Function for the matlab cross validation.
+    %
+    %             Has to be curried to bind order and classifier
+    %             beforehands.
+    %
+    %%  Input:
+    %       trainFeatures ... a 3-dimensional matrix of instances for the
+    %                         classifier to be trained on
+    %       trainLabels ..... a 2-dimensional matrix of lables of the
+    %                         training instances
+    %       evalFeatures .... a 3-dimensional matrix of instances for the
+    %                         classifier to be evaluated on
+    %       evalLabels ...... a 2-dimensional matrix of lables of the
+    %                         evaluation instances
+    %       order ........... order of the classes in the confusion
+    %                         matrix
+    %       classifier ...... classifier to be trained and evaluated
+    %
+    %%  Output:
+    %       confMat ..........a confusion matrix yielded by evaluating the
+    %                         classifier 
+    %%
 
     %Learn classifier on trainFeatures/Labels and classify evalFeatures
     classifier = classifier.trainOn(trainFeatures, trainLabels);
