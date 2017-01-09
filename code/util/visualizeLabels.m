dc411bbf64bd8b8206c50e7111dfd189aea6a1c6
crekowski <cornelius.vonrekowski@gmail.com>
2017-01-09 11:44:22 +0100

    Only use one max() call in visualizeLabels

--- code/util/visualizeLabels.m
+++ code/util/visualizeLabels.m
@@ -24,7 +24,7 @@ function visualizeLabels(labelMap, name)
     % - fill pixels: black
     % - unlabeled: white
     % - labeled: colors
-    maxClass = max(max(labelMap));
+    maxClass = max(labelMap(:));
     colors = [0 0 0; 1 1 1; hsv(maxClass)];
     
     % Add 2 to the label map, because there might be -1 and 0 labels
